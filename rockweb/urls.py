5577b031f6ada329dca21d6ad4f908b9f7a06215
aarock766 <aarock766@gmail.com>
2022-04-07 17:00:47 +0800

    Office 20220407

--- rockweb/urls.py
+++ rockweb/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mymoney/', include(mymoney(mymoney.ruls))),
-]
+    path('index/',index)
+    ]
